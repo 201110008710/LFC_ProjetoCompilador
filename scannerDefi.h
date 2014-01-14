9aee4661501392579dcdacf15c3e68ad1d070c6b
Jose Elwyslan Mauricio de Oliveira <Elwyslan@Elwyslan-OpenSuse.elwyslan>
2014-01-14 03:57:21 -0300

    Adicionado o cabeçalho que contem algumas definições e a primeira versão do scanner que reconhece numeros(NUM) e indentificadores(ID)

---
+++ scannerDefi.h
@@ -0,0 +1,35 @@
+//Definicoes Lexicas
+#define ID 1
+#define NUM 2
+#define STRING 3
+
+//Palavras reservadas
+#define ELSE 4
+#define	IF 5
+#define	INT 6
+#define RETURN 7
+#define VOID 8
+#define WHILE 9
+
+//Simbolos especiais
+#define MAIS 10
+#define MENOS 11
+#define ASTERISCO 12
+#define BARRA 13
+#define MENOR 14
+#define MENOR_IGUAL 15
+#define MAIOR 16
+#define MAIOR_IGUAL 17
+#define IGUAL_IGUAL 18
+#define DIFERENTE 19
+#define IGUAL 20
+#define PONTO_E_VIRGULA 21
+#define VIRGULA 22
+#define ABRE_PARENTESE 23
+#define FECHA_PARENTESE 24
+#define ABRE_COLCHETE 25
+#define FECHA_COLCHETE 26
+#define ABRE_CHAVE 27
+#define FECHA_CHAVE 28
+#define BARRA_ASTERISCO 29
+#define ASTERISCO_BARRA 30
